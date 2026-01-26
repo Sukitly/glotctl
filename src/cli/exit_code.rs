9b5ccf5b00e2e8a0ff8aecdae9cf078420a65d98
Shuqian <sukitly@gmail.com>
2026-01-26 17:33:03 +0800

    update

--- src/cli/exit_code.rs
+++ src/cli/exit_code.rs
@@ -1,9 +1,7 @@
 use super::commands::CommandResult;
 
 pub fn exit_code_from_result(result: &CommandResult) -> i32 {
-    if result.parse_error_count > 0 {
-        1
-    } else if result.exit_on_errors && result.error_count > 0 {
+    if result.parse_error_count > 0 || (result.exit_on_errors && result.error_count > 0) {
         1
     } else {
         0
